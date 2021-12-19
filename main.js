b061b71aacaaa5960478bd6a78c94ccce8d9cdfc
guswnsakvk <guswnsakvk1@naver.com>
2021-12-19 21:27:08 +0900

    friend hidden

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const profile = document.getElementById("profile")
+const friend = document.getElementById("friend")
 
 window.addEventListener("resize", (event) => {
   if(event.target.innerWidth > 768){
     profile.classList.add("border-start")
     profile.classList.add("border-2")
+    friend.classList.remove("hidden")
   }
   else{
     profile.classList.remove("border-start")
     profile.classList.remove("border-2")
+    friend.classList.add("hidden")
   }
 })
